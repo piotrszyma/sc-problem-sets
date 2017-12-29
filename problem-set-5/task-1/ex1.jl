9ce35ff715ebd13784cbc0d8f442389b76fff3f1
Piotr Szyma <thompson2908@gmail.com>
2017-12-29 15:26:16 +0100

    Input reading introduced

--- problem-set-5/task-1/ex1.jl
+++ problem-set-5/task-1/ex1.jl
@@ -1,51 +1,100 @@
 # Read from file
 
+function generate_entry(values_list, size)
+  parsed_list = []
+  for v in values_list
+    x, y, value = split(v)
+    x = parse(Int64, x)
+    y = parse(Int64, y)
+    value = parse(Float64, value)
+    push!(parsed_list, [x, y, value])
+  end
+
+  A_start = 1
+  B_start = A_start + 2 * size
+  C_start = B_start + size * size
+  C_vals = parsed_list[A_start:A_start+2*size-1]
+  A_vals = parsed_list[B_start:B_start+size*size-1]
+  B_vals = parsed_list[C_start:end]
+
+  A = zeros(Float64, size, size)
+  B = zeros(Float64, size, size)
+  C = zeros(Float64, size, size)
+  
+  counter = 1
+  for i in 1:4
+    for j in 1:4
+      x, y, value = A_vals[counter]
+      x = trunc(Int64, x)
+      y = trunc(Int64, y)
+      A[i, j] = value
+      counter+=1
+    end
+  end
+
+  println(A)
+
+  counter = 1
+  for j in 1:4
+    x, y, value = B_vals[counter]
+    x = trunc(Int64, x)
+    y = trunc(Int64, y)
+    B[j, j] = value
+    counter+=1
+  end
+
+  counter = 1
+
+  for i in 1:4
+    for j in 3:4
+      x, y, value = C_vals[counter]
+      x = trunc(Int64, x)
+      y = trunc(Int64, y)
+      C[i, j] = value
+      counter+=1
+    end
+  end
+
+  return [sparse(x) for x in [A, B, C]]
+end
+
 function read_matrix_from_file(filename)
   open(filename) do f
     lines = readlines(f)
+  end
 
     n, l = split(lines[1])
     n, l = parse(Int, n), parse(Int, l)
 
     v = n / l
+    lines = [["0 0 0.0" for i in 1:2*l]; lines[2:end]; ["0 0 0.0" for i in 1:l];]
 
-    R = [[0.0 for _ in 1:n] for _ in 1:n]
-    # INPUT = []
-    for line in lines[2:end]
-      x, y, value = split(line)
-      x = parse(Int64, x)
-      y = parse(Int64, y)
-      value = parse(Float64, value)
-      R[x][y] = value
+    parsed_input = []
+
+    for index in 1:(l+3)*l:length(lines)
+      A, B, C = generate_entry(lines[index:index + l*(l+3) - 1], l)
+      push!(parsed_input, [C, A, B])
     end
-    for (i, r) in enumerate(1:n)
-        println(R[r])
-    end
-    shift = -1;
-    for (i, r) in enumerate(1:4:n)
-        # println(r)
-        for x in r:r+3
-          println(shift)
-          println(x)
-        end
-        shift += 1;
-    end
-    
-    # first = INPUT[1:(l * (l + 1))]
-    # middle = INPUT[(l * (l + 1)) + 1:(l * (l + 2)) - 1]
-    # println(last)    
-  end
+
+    return parsed_input
 end
 
-function read_vector_from_file(filename)
+function read_vector_from_file(filename, v)
+  lines = []
   open(filename) do f
-    lines = readlines(f)
-    n, l = lines[1]
-    for l in lines[2:end]
-      v = l
-    end
+    lines = [parse(Float64, l) for l in readlines(f)]
   end
+  n = trunc(Int, lines[1])
+  l = trunc(Int, n / v)
+  vector = []
+  for block in 1:l:n
+    push!(vector, lines[block:block+l-1])
+  end
+  return vector
 end
 
-read_matrix_from_file("../input/A.txt");
-# read_vector_from_file("../input/b.txt");
+matrix = read_matrix_from_file("../input/A.txt");
+
+vector = read_vector_from_file("../input/b.txt", length(matrix))
+
+println(vector)
